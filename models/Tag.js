4f871a9787a64f3dd01b4e70eb6e7b4e6da66c76
katredford <katharine.redford@gmail.com>
2021-06-21 15:22:20 -0500

    add tag model

--- models/Tag.js
+++ models/Tag.js
@@ -5,8 +5,20 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
-  {
-    // define columns
+  { //this is the ID column
+    id: {
+      // this means the type of data in this column is a number
+      type: DataTypes.INTEGER,
+      //this means something has to go in this column, it cannot be empty
+      allowNull: false,
+      //
+      primaryKey: true,
+      //this automatically adds one to the id when it is created
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
---
+++ notes.md
@@ -0,0 +1,4 @@
+mysql -u root -p
+npm init
+npm install express sequelize mysql2
+npm install dotenv
